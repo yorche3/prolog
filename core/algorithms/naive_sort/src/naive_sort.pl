e45f77e1fc2ff7b22ce73793c3b7bf474cf29d3e
yorche3 <hyaoki123@gmail.com>
2026-09-18 17:49:12 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.pl
+++ core/algorithms/naive_sort/src/naive_sort.pl
@@ -6,3 +6,36 @@
 %   selection_sort(+Lista, -Ordenada)
 %   bubble_sort(+Lista, -Ordenada)
 %   insertion_sort(+Lista, -Ordenada)
+
+selection_sort([], []).
+selection_sort([X|Xs], Ordenada) :-
+    selection_sort(Xs, OrdenadaXs),
+    insert_min(X, OrdenadaXs, Ordenada).
+
+insert_min(X, [], [X]).
+insert_min(X, [Y|Ys], [X,Y|Ys]) :- X =< Y.
+insert_min(X, [Y|Ys], [Y|Zs]) :-
+    X > Y,
+    insert_min(X, Ys, Zs).
+
+bubble_sort([], []).
+bubble_sort([X|Xs], Ordenada) :-
+    bubble_sort(Xs, OrdenadaXs),
+    insert_bubble(X, OrdenadaXs, Ordenada).
+
+insert_bubble(X, [], [X]).
+insert_bubble(X, [Y|Ys], [X,Y|Ys]) :- X =< Y.
+insert_bubble(X, [Y|Ys], [Y|Zs]) :-
+    X > Y,
+    insert_bubble(X, Ys, Zs).
+
+insertion_sort([], []).
+insertion_sort([X|Xs], Ordenada) :-
+    insertion_sort(Xs, OrdenadaXs),
+    insert(X, OrdenadaXs, Ordenada).
+
+insert(X, [], [X]).
+insert(X, [Y|Ys], [X,Y|Ys]) :- X =< Y.
+insert(X, [Y|Ys], [Y|Zs]) :-
+    X > Y,
+    insert(X, Ys, Zs).
